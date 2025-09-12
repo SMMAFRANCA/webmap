1268948dbd28f9b817751d49e0469a6aaf59ecfe
Helio Mendes <heliomendes@franca.sp.gov.br>
2025-09-12 07:42:54 -0300

    adicionado novo ponto viciado no res olavo pinheiro

--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -9,7 +9,7 @@ var map = new ol.Map({
 });
 
 //initial view - epsg:3857 coordinates if not "Match project CRS"
-map.getView().fit([-5297828.883140, -2348107.637120, -5254747.792935, -2327449.049563], map.getSize());
+map.getView().fit([-5288337.376488, -2347975.467094, -5264231.265000, -2327597.271005], map.getSize());
 
 ////small screen definition
     var hasTouchScreen = map.getViewport().classList.contains('ol-touch');
