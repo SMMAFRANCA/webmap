7d0e65b9291baf10fa23cda9bc79a46479044a60
Helio Mendes <heliomendes@franca.sp.gov.br>
2026-03-30 13:01:40 -0300

    novos pontos viciados particulares

--- styles/reaVerde1946unid805735618m_48_style.js
+++ styles/reaVerde1946unid805735618m_48_style.js
@@ -1,7 +1,7 @@
 var size = 0;
 var placement = 'point';
 
-var style_ImvelPrivado93unid_329 = function(feature, resolution){
+var style_reaVerde1946unid805735618m_48 = function(feature, resolution){
     var context = {
         feature: feature,
         variables: {}
@@ -21,8 +21,7 @@ var style_ImvelPrivado93unid_329 = function(feature, resolution){
         labelText = String("");
     }
     var style = [ new ol.style.Style({
-        image: new ol.style.Circle({radius: 8.0 + size,
-            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(216,92,121,1.0)'})}),
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.608}),fill: new ol.style.Fill({color: 'rgba(69,178,22,0.5019607843137255)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
