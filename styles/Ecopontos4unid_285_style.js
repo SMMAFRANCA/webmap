4c0b2d64ef887130a8c3aec1fb329346b3bc0c69
Helio Mendes <heliomendes@franca.sp.gov.br>
2026-04-27 10:27:57 -0300

    acrescentei a informação área institucional na quadra 13 do prol do jardim antonio petraglia ii

--- styles/Ecopontos4unid_285_style.js
+++ styles/Ecopontos4unid_285_style.js
@@ -1,7 +1,7 @@
 var size = 0;
 var placement = 'point';
 
-var style_Ecopontos_285 = function(feature, resolution){
+var style_Ecopontos4unid_285 = function(feature, resolution){
     var context = {
         feature: feature,
         variables: {}
