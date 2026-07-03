fedb0a56b9828f38e7d9d067ecd02b1343902169
Helio Mendes <heliomendes@franca.sp.gov.br>
2026-07-03 14:38:19 -0300

    alterada simbologia dos ecopontos para quadrado

--- styles/Emfuncionamento4unid_360_style.js
+++ styles/Emfuncionamento4unid_360_style.js
@@ -23,8 +23,8 @@ var style_Emfuncionamento4unid_360 = function(feature, resolution){
         labelText = String("");
     }
     var style = [ new ol.style.Style({
-        image: new ol.style.Circle({radius: 8.0 + size,
-            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(2,235,132,1.0)'})}),
+        image: new ol.style.RegularShape({radius: 12.0 + size, points: 4,
+            angle: Math.PI/4, displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(2,253,235,1.0)'})}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
