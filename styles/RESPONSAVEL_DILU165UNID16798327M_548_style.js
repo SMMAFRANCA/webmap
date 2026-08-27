db9ca3cfb0c359791cefb6151753e1a796d23163
Helio Mendes <heliomendes@franca.sp.gov.br>
2026-08-27 16:18:58 -0300

    atualização do adote uma praça 27/08/2026

--- styles/RESPONSAVEL_DILU165UNID16798327M_548_style.js
+++ styles/RESPONSAVEL_DILU165UNID16798327M_548_style.js
@@ -1,7 +1,7 @@
 var size = 0;
 var placement = 'point';
 
-var style_RESPONSAVEL_EGNALDO217UNID21711881M_547 = function(feature, resolution){
+var style_RESPONSAVEL_DILU165UNID16798327M_548 = function(feature, resolution){
     var context = {
         feature: feature,
         variables: {}
@@ -24,7 +24,7 @@ var style_RESPONSAVEL_EGNALDO217UNID21711881M_547 = function(feature, resolution
     }
     var style = [ new ol.style.Style({
         image: new ol.style.Circle({radius: 8.0 + size,
-            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(231,113,72,1.0)'})}),
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(190,207,80,1.0)'})}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
