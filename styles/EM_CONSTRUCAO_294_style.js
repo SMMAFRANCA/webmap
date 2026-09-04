e3797ba35b148b5f2fb61fd3b29d683779baa05e
Helio Mendes <heliomendes@franca.sp.gov.br>
2026-09-04 12:46:01 -0300

    2 novos loteamentos clandestinos

---
+++ styles/EM_CONSTRUCAO_294_style.js
@@ -0,0 +1,34 @@
+var size = 0;
+var placement = 'point';
+
+var style_EM_CONSTRUCAO_294 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "9.1px \'Arial\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "#fafafa";
+    var bufferWidth = 3.0;
+    var textAlign = 'left';
+    var offsetX = 8;
+    var offsetY = 3;
+    var overflow = false;
+    var repeat = 0;
+    var placement = 'point';
+    if (feature.get("NOME") !== null) {
+        labelText = String(feature.get("NOME"));
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 12.0 + size, points: 4,
+            angle: Math.PI/4, displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(255,0,34,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
+    })];
+
+    return style;
+};
