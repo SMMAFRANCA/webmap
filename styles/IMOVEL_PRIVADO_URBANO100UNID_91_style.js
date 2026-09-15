d23a2e85f8b6a420be21f82510be744d5108983d
Helio Mendes <heliomendes@franca.sp.gov.br>
2026-09-15 12:35:33 -0300

    ponto viciado na rua miragaia mmdc

--- styles/IMOVEL_PRIVADO_URBANO100UNID_91_style.js
+++ styles/IMOVEL_PRIVADO_URBANO100UNID_91_style.js
@@ -1,7 +1,7 @@
 var size = 0;
 var placement = 'point';
 
-var style_IMOVEL_PRIVADO_URBANO100UNID_92 = function(feature, resolution){
+var style_IMOVEL_PRIVADO_URBANO100UNID_91 = function(feature, resolution){
     var context = {
         feature: feature,
         variables: {}
@@ -23,8 +23,7 @@ var style_IMOVEL_PRIVADO_URBANO100UNID_92 = function(feature, resolution){
         labelText = String("");
     }
     var style = [ new ol.style.Style({
-        image: new ol.style.Circle({radius: 8.0 + size,
-            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(216,92,121,1.0)'})}),
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(253,191,111,0.5019607843137255)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
--- styles/IMOVEL_PRIVADO_URBANO101UNID_92_style.js
+++ styles/IMOVEL_PRIVADO_URBANO101UNID_92_style.js
@@ -1,7 +1,7 @@
 var size = 0;
 var placement = 'point';
 
-var style_IMOVEL_PRIVADO_URBANO99UNID_91 = function(feature, resolution){
+var style_IMOVEL_PRIVADO_URBANO101UNID_92 = function(feature, resolution){
     var context = {
         feature: feature,
         variables: {}
@@ -23,7 +23,8 @@ var style_IMOVEL_PRIVADO_URBANO99UNID_91 = function(feature, resolution){
         labelText = String("");
     }
     var style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(253,191,111,0.5019607843137255)'}),
+        image: new ol.style.Circle({radius: 8.0 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(216,92,121,1.0)'})}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
