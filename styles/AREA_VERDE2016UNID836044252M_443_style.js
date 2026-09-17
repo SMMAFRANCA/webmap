b8ebd5ee0ccc819e5d54d1f4f875ab792e3cdb31
Helio Mendes <heliomendes@franca.sp.gov.br>
2026-09-17 10:08:56 -0300

    novos loteamentos clandestinos 16/09/2026

--- styles/AREA_VERDE2016UNID836044252M_443_style.js
+++ styles/AREA_VERDE2016UNID836044252M_443_style.js
@@ -1,7 +1,7 @@
 var size = 0;
 var placement = 'point';
 
-var style_IRREGULAR179UNID_254 = function(feature, resolution){
+var style_AREA_VERDE2016UNID836044252M_443 = function(feature, resolution){
     var context = {
         feature: feature,
         variables: {}
@@ -9,22 +9,21 @@ var style_IRREGULAR179UNID_254 = function(feature, resolution){
     
     var labelText = ""; 
     var value = feature.get("");
-    var labelFont = "10.4px \'Arial\', sans-serif";
-    var labelFill = "#323232";
-    var bufferColor = "#fafafa";
-    var bufferWidth = 3.0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
     var textAlign = 'left';
     var offsetX = 8;
     var offsetY = 3;
     var overflow = false;
     var repeat = 0;
     var placement = 'point';
-    if (feature.get("NOME_LOTEAMENTO") !== null) {
-        labelText = String(feature.get("NOME_LOTEAMENTO"));
+    if ("" !== null) {
+        labelText = String("");
     }
     var style = [ new ol.style.Style({
-        image: new ol.style.Circle({radius: 8.0 + size,
-            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(229,7,46,1.0)'})}),
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.608}),fill: new ol.style.Fill({color: 'rgba(69,178,22,0.5019607843137255)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
